andreubotella/deno_test262

commit c445b82a56d2c5d3b60c8d6c95e650636259ab06

Implement `$262.evalScript()`. (#25)
Closes #22.

runner_util/setup.js
@@ -2,7 +2,7 @@
   // Make sure we control the only output.
   const { Deno, console, URL } = globalThis;
   delete globalThis.Deno;
-  delete globalThis.console;
+  //delete globalThis.console;
   delete globalThis.URL;
 
   const liftThis = (fn) => fn.call.bind(fn);
@@ -77,6 +77,16 @@
       };
     });
   }
+
+  globalThis.$262 = {
+    evalScript(script) {
+      const [ret, err] = Deno.core.evalContext(script);
+      if (err !== null) {
+        throw err.thrown;
+      }
+      return ret;
+    },
+  };
 
   const test262Root = new URL("../test262/", import.meta.url);
 

commit dc90c6c4ca67a5eb6c94fc8f93c522ab0f5446f6

Don't expose the `console` global to the test environment
This bug was introduced by mistake in #25.

runner_util/setup.js
@@ -2,7 +2,7 @@
   // Make sure we control the only output.
   const { Deno, console, URL } = globalThis;
   delete globalThis.Deno;
-  //delete globalThis.console;
+  delete globalThis.console;
   delete globalThis.URL;
 
   const liftThis = (fn) => fn.call.bind(fn);